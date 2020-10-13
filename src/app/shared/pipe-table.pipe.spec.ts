e539a30d61ab15b3a8327ad50f6ebd9d38385757
girishreddy-infrrd <69350088+girishreddy-infrrd@users.noreply.github.com>
2020-10-13 20:25:55 +0530

    Revert "INITIAL CODE"
    This reverts commit a4ccf53048ce7486b27c636f68b251892f878fe6.

--- src/app/shared/pipe-table.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PipeTablePipe } from './pipe-table.pipe';
-
-describe('PipeTablePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PipeTablePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
